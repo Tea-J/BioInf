9f41a840f77d9f3ca7169de8ba46d30674c1a054
stuka11 <sergej_hajdin@hotmail.com>
2014-12-29 00:16:12 +0100

    RobinKarp

--- sh44794/RabinKarp.java
+++ sh44794/RabinKarp.java
@@ -5,7 +5,13 @@
  */
 package bioinformatika;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,44 +26,93 @@ public class RabinKarp {
 
     private byte[] genomeData = null;
     private byte[] sequenceData = null;
-    private Path genome = null;
-    private Path sequence = null;
-    private int prime = 101;
-    
+    private Path genomePath = null;
+    private Path sequencePath = null;
+    private int prime = 7;
+    private Path genomePath2 = null;
+    private Path sequencePath2 = null;
+    private int sequenceSize;
+    private int genomeSize;
+    private long sequenceHash;
+    private int hashHits = 0;
+    private int actualHits = 0;
 
+    //TODO provjeriti usporedbe int == int, byte == byte ili mora biti equals, razlika u brzini
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
         //File file = new File("C:\\Users\\serz\\Desktop\\BIOINF\\PROJEKT\\Escherichia_coli_asm59784v1.GCA_000597845.1.24.dna.toplevel.fa");
-        Path genomePath = Paths.get("C:\\Users\\serz\\Desktop\\BIOINF\\PROJEKT\\tekst.txt");
+        Path genomePath = Paths.get("C:\\Users\\serz\\Desktop\\BIOINF\\PROJEKT\\Escherichia_coli_asm59784v1.GCA_000597845.1.24.dna.toplevel.fa");
         Path sequencePath = Paths.get("C:\\Users\\serz\\Desktop\\BIOINF\\PROJEKT\\tekst.txt");//
-        int primeNum = 101;
-        if(args.length == 3) {
+
+        long start = System.currentTimeMillis();
+
+         int primeNum = 101;
+        if (args.length == 3) {
             try {
                 primeNum = Integer.parseInt(args[2]);
-            }catch(NumberFormatException e){
-                System.out.println("Treći argument nije broj");
+            } catch (NumberFormatException e) {
+                System.out.println("Third argument is not a number!");
+                return;
             }
         }
-        
+
         RabinKarp rabinKarp = new RabinKarp(genomePath, sequencePath, primeNum);
+        rabinKarp.hash(1);
+        rabinKarp.prepareFiles();
         rabinKarp.readFiles();
+        long end = System.currentTimeMillis();
+        System.out.println("Time elapsed: " + (end - start) + "ms");
+
         rabinKarp.runRabinKarp();
     }
-    
-    RabinKarp(Path genome, Path sequence, int prime){
-        this.genome = genome;
-        this.sequence = sequence;
+
+    RabinKarp(Path genomePath, Path sequencePath, int prime) {
+        this.genomePath = genomePath;
+        this.sequencePath = sequencePath;
         this.prime = prime;
     }
 
-    
-    void readFiles(){
+    void prepareFiles() {
+        String gP = genomePath.toString();
+        String sP = sequencePath.toString();
+
+        int dot = gP.lastIndexOf('.');
+        genomePath2 = Paths.get(gP.substring(0, dot) + "_edited" + gP.substring(dot));
+        dot = sP.lastIndexOf('.');
+        sequencePath2 = Paths.get(sP.substring(0, dot) + "_edited" + sP.substring(dot));
+
+        storeEditedFile(genomePath, genomePath2);
+        storeEditedFile(sequencePath, sequencePath2);
+    }
+
+    void storeEditedFile(Path inPath, Path outPath) {
+        Charset charset = StandardCharsets.ISO_8859_1;
+        try (BufferedReader reader = Files.newBufferedReader(inPath, charset)) {
+            FileOutputStream fos = new FileOutputStream(outPath.toFile());
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos, charset));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if (!line.startsWith(">")) {
+                    bw.write(line);
+                }
+            }
+            reader.close();
+            bw.close();
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+    }
+
+    void readFiles() {
         try {
-            genomeData = Files.readAllBytes(genome);
-            sequenceData = Files.readAllBytes(sequence);
+            genomeData = Files.readAllBytes(genomePath2);
+            genomeSize = genomeData.length;
+            sequenceData = Files.readAllBytes(sequencePath2);
+            sequenceSize = sequenceData.length;
         } catch (IOException ex) {
             Logger.getLogger(RabinKarp.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Error reading from files!");
@@ -65,15 +120,45 @@ public class RabinKarp {
     }
 
     private void runRabinKarp() {
-        
+        long genomeHash = initHash();
+        int i = 0;
+        while(i <= (genomeSize - sequenceSize)){
+            if(genomeHash == sequenceHash){
+                hashHits++;
+                if(checkPattern(i)){
+                    actualHits++;
+                    //vrati odmah da ima ili broji kolko ih ima ili spremi pocetne indexe svakog ponavljanja
+                }
+            }
+            genomeHash = hash(++i); // paziti na zadnjeg da en cita iz elementa većeg od veličine polja
+        }
     }
-    
-    int hash (byte[] pattern, int start, int end){
-        int hash = 0;
+
+    long hash(int start) {
+        long hash = 0;
         
+
+        return hash;
+    }
+
+    long initHash() {
+        long seqhash = 0;
+        long hash = 0;
+        for(int i = 0; i < sequenceSize ; ++i){
+
+            
+        }
+        sequenceHash = seqhash;
         return hash;
     }
     
+    boolean checkPattern(int start){
+        boolean b = true;
+        for(int i = 0; i < sequenceSize; ++i){
+            if(sequenceData[i] != genomeData[start + i]){
+               return false;
+            }
+        }        
+        return b;
+    }
 }
-
-
