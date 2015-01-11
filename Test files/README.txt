6d80b094435b6510ce989d6d775d064e305b2775
Tea-J <tjerman711@gmail.com>
2015-01-11 23:14:35 +0100

    Rabin Karp
    - prepravljen hash i search
    - to do: rijesiti sporost
    - dodane testne datoteke

---
+++ Test files/README.txt
@@ -0,0 +1,3 @@
+broj_podnizova	duljina _podnizova
+prazan red
+podnizovi, svaki u novom redu, bez dodatnih znakova (razmaka isl)
--- tj45512/RabinKarp.c
+++ tj45512/RabinKarp.c
@@ -7,75 +7,92 @@
 #include <time.h>
 #include <errno.h>
 
-#define base 101
 #define N 80
+
+int detectedHash, correctHash = 0;
 
 
 int Ascii(char c){
 	return c;
 }
 
-long Hash(char *substring){
-	int i, lsubstring;
+long Hash(long oldhash, char *substring, char oldbyte){
+	int i, sum, lsubstring;
+	char newbyte;
 	long hash_value;
 	
 	lsubstring = strlen(substring);
-	
 	hash_value = 0;
 
-	for (i = 0; i < lsubstring; i++){
-		hash_value += (Ascii(*(substring + i)))*((long)(pow((double)base, lsubstring - i - 1)));
+	if (!oldbyte){
+		for(i=0;i<lsubstring; i++){
+			hash_value += *(substring+i) * (lsubstring-i-1);
+		}
+	}
+	else {
+		sum = 0;
+		for (i=0; i<lsubstring; i++){
+			sum+=*(substring+i);
+		}
+		newbyte = *(substring + lsubstring -1);
+		hash_value = oldhash + sum - lsubstring*Ascii(oldbyte);
 	}
 
 	return hash_value;
 }
 
-int Search(char* string, char *patterns[], int num_patterns, int lpatterns){
-	int lstring, i, j, return_value;
-	long hsubstring;
-	long *hpattern;
-	char *substring;
+int Check(char *A, char *B){
+	if( strcmp(A, B) == 0 )
+		return 1;	//Strings are equal
+	else
+		return 0;	//Strings are not equal
+}
+
+int *Search(char* substring, char *patterns[], int num_patterns, int lpatterns, char oldbyte){
+	int lstring, i, result;
+	static long hsubstring = 0;
+	long *hpattern = 0;
+	int *return_value;
 
 	hpattern = (long *)malloc(num_patterns*sizeof(long));
+	return_value = (int *)malloc(num_patterns*sizeof(int));
 
 	for(i = 0; i < num_patterns; i++) {
-		*(hpattern+i) = Hash(patterns[i]);
+		*(hpattern+i) = Hash(0, patterns[i], 0);
+		*(return_value+i) = 0;
     }
 	
-	lstring = strlen(string);
-	substring = (char *)malloc((lpatterns + 1)*sizeof(char));
+	lstring = strlen(substring);
 
-	return_value = -1;	//if no substring was found
+	for (i = 0; i < num_patterns; i++){
+		if (hsubstring == *(hpattern+i)){
+			detectedHash++;
+			result = Check(substring, patterns[i]);
 
-	for (i = 0; i < lstring - lpatterns + 1; i++){
-		strncpy_s(substring, lpatterns + 1, string + i, lpatterns);
-		hsubstring = Hash(substring);
-
-		for(j = 0; j < num_patterns; j++){
-			if (hsubstring == *(hpattern+j)){
-				return_value = i;
-				break;
+			if (result){
+				correctHash++;
+				*(return_value+i) = 1;
 			}
-		}
-
-		if (return_value != (-1)){
-			break;
 		}
 	}
 
-	free(substring);
 	return return_value;
 }
 
+
 int main(){
 	FILE *genome; 
-	int result, lbuffer, offset, flag, errnum;
+	int i, j, lbuffer, offset, flag, errnum;
+	char oldbyte = 0;
 	char *buffer;
 	char *string;
-	//char *string = "abrakadabra";
+	char *substring;
+	int *result;
+	int num_patterns = 4;
+	int lpattern = 10;
+	float efficiency;
+
 	char *subs[] = { "aka", "TAATATGCAA", "dab", "ada" };
-	//max lenght of a substring is 4 characters
-	//i.e. for "TTTTT" we don't get correct hash value
 
 	time_t start, end;
 	double dif;
@@ -90,60 +107,70 @@ int main(){
 	}
 
 	buffer = (char *)malloc(N*sizeof(char));
-	string = (char *)malloc(N*sizeof(char));
-	flag = 0;	//from the start
+	result = (int *)malloc(num_patterns*sizeof(int));
+	substring = (char *)malloc(lpattern*sizeof(char));
+	
+	flag = 1;	//from the start
 	offset = 0;
-	result = -1;
+
+	for (i=0; i<num_patterns; i++)
+		*(result+i) = 0;
 
 	time(&start);
 	
 	while (fgets(buffer, N, genome)){
-
 		if (*buffer == '>'){
-			flag = 0;
 			continue;
+			flag = 1;
 		}
 
-		//printf("buffer:\n%s", buffer);
-		lbuffer = strlen(buffer)-1;	//when using fgets, strlen counts '\n'
+		lbuffer = strlen(buffer);
 
-		if (flag == 0){
+		if (flag){
+			string = (char *)malloc(lbuffer*sizeof(char));
 			strncpy_s(string, lbuffer+1, buffer, lbuffer);
-			result = Search(string, subs, 4, 10);
-			if (result >= 0){
-				break;
-			}
-			flag=1;
-		} 
-		else {
-			strncpy_s(string, 3, string+strlen(string)-2, 2);
-			strncpy_s(string+2, lbuffer+1, buffer, lbuffer);
-			result = Search(string, subs, 4, 10);
-			if (result >= 0){
-				result=result-2;
-				break;
-			}
+			flag = 0;
 		}
-
-		offset += lbuffer;
-		//printf("offset + lbuffer %d\n", offset);
+		else{
+			offset = strlen(string)-1;
+			string = (char *)realloc(string, offset+lbuffer*sizeof(char));
+			strncpy_s(string+offset, lbuffer+1, buffer, lbuffer);
+		}
 	}
 
 	time(&end);
 	dif = difftime(end, start);
-	
-	if (result < 0){
-		printf("No results.\n");
-	}
-	else{
-		printf("A substring was found at index %d.\n", offset + result);
-	}
 
+	printf("Processing input file took %.2lf seconds.\n", dif);
+	
+	time(&start);
+	
+	for (i = 0; i<strlen(string);i++){
+		strncpy_s(substring, lpattern+1, string, lpattern);
+		result = Search(substring, subs, num_patterns, lpattern, oldbyte);
+		oldbyte= *substring;
+
+		for(j = 0; j < num_patterns; j++){
+			if (*(result+j)){
+				printf("%d. substring found at index %d.\n", j, i);
+			}
+		}
+	}
+	
+	time(&end);
+	dif = difftime(end, start);
+
+	if (detectedHash)
+		efficiency = (correctHash/detectedHash)*100;
+	else
+		efficiency = 0;
+
+	printf("\nDetected: %d\nCorrect: %d\nEfficiency: %.2f %\n", detectedHash, correctHash, efficiency);
 	printf ("Calculations took %.2lf seconds to run.\n", dif );
 	
 	fclose(genome);
 	free(buffer);
-	free(string);
+	//free(string);
 	getchar();
 
 	return 0;
