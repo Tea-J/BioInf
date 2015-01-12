d7e7ebf5406eb1d1c8ae8ebe87c95da82e3f573e
martinblaz <martin.blazevic@fer.hr>
2015-01-12 20:56:34 +0100

    dodano mjerenje vremena

--- mb45320/RabinKarp.cs
+++ mb45320/RabinKarp.cs
@@ -11,10 +11,9 @@ namespace Bioinformatika
     static class RabinKarp
     {
         const ulong BASE = 11;
-        //const ulong HASH_SIZE = (2 << 20);
+        const ulong HASH_SIZE = (2 << 20);
 
-		
-		public static void Main(string[] args)
+        public static void Main(string[] args)
         {
             const int NUMBER_OF_INPUT_ARGUMENTS = 2;
             const int GENOME_FILE_INDEX = 0;
@@ -28,8 +27,8 @@ namespace Bioinformatika
                 return;
             }
 
+            Console.WriteLine("Attempting to verify the genome file...");
             var genomeFilePath = args[GENOME_FILE_INDEX];
-
             if (!File.Exists(genomeFilePath))
             {
                 Console.WriteLine("Genome file does not exist!");
@@ -48,8 +47,8 @@ namespace Bioinformatika
                 return;
             }
 
+            Console.WriteLine("Attempting to verify the pattern file...");
             var patternFilePath = args[PATTERN_FILE_INDEX];
-
             if (!File.Exists(patternFilePath))
             {
                 Console.WriteLine("Pattern file does not exist!");
@@ -67,16 +66,26 @@ namespace Bioinformatika
                 Console.WriteLine("Could not open the pattern file!");
                 return;
             }
-			
-            var genome = ReadGenomeFromFile(genomeFilePath);
 
+            Console.WriteLine("Reading the genome file...");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Reset();
+            stopwatch.Start();
+            var genome = ReadGenomeFromFile(genomeFilePath);
+            stopwatch.Stop();
+            Console.WriteLine("Reading the genome file took {0} ms.", stopwatch.ElapsedMilliseconds);
             if (string.IsNullOrEmpty(genome))
             {
                 Console.WriteLine("Genome cannot be an empty string!");
                 return;
             }
 
+            Console.WriteLine("Reading the pattern file...");
+            stopwatch.Reset();
+            stopwatch.Start();
             var patterns = ReadPatternsFromFile(patternFilePath);
+            stopwatch.Stop();
+            Console.WriteLine("Reading the pattern file took {0} ms.", stopwatch.ElapsedMilliseconds);
 
             if (patterns.Length == 0)
             {
@@ -86,7 +95,10 @@ namespace Bioinformatika
 
             if (patterns.Length == 1)
             {
+                Console.WriteLine("Starting the search...");
+                Console.WriteLine();
                 var instances = Search(genome, patterns[0]);
+                Console.WriteLine();
                 if (instances.Count > 0)
                 {
                     Console.Write("Pattern instances found on the following indices: ");
@@ -108,7 +120,8 @@ namespace Bioinformatika
 
             Console.WriteLine("Multiple  patterns  not supported... :(");
         }
-		
+
+
         private static string ReadGenomeFromFile(string filePath)
         {
             return File.ReadAllText(filePath);
@@ -151,9 +164,8 @@ namespace Bioinformatika
             return patterns.ToArray();
         }
 
-        private static List<int> Search(string genome, string pattern) 
+        private static List<int> Search(string genome, string pattern)
         {
-
             if (ReferenceEquals(genome, null))
                 throw new ArgumentNullException("genome", "Genome cannot be null!");
 
@@ -169,23 +181,38 @@ namespace Bioinformatika
             if (genome.Length < pattern.Length)
                 throw new ArgumentException("Genome cannot be shoter than the pattern.");
 
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Reset();
+
             var patternInstances = new List<int>();
 
-            ulong patternLength = (ulong) pattern.Length, genomeLength = (ulong) genome.Length;
+            ulong patternLength = (ulong)pattern.Length, genomeLength = (ulong)genome.Length;
 
+            stopwatch.Start();
             var hashes = CalculateInitialHash(genome, pattern, patternLength, BASE);
+            stopwatch.Stop();
+            Console.WriteLine("Calculating the initial hashes took {0} ms or {1} timer ticks.",
+                stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks);
 
             ulong genomeHash = hashes.Item1, patternHash = hashes.Item2;
 
+            // check for match at offset 0
             if (genomeHash == patternHash)
             {
+                stopwatch.Reset();
+                stopwatch.Start();
                 if (IsSubstring(genome, pattern, 0))
                     patternInstances.Add(0);
+                stopwatch.Stop();
+                Console.WriteLine("Comparison of pattern and substring took {0} ms or {1} timer ticks.",
+                    stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks);
             }
-			
-			ulong factor = CalculateFactor(patternLength, BASE);
 
-            for (int i = (int) patternLength; i < (int) genomeLength; i++)
+            ulong factor = CalculateFactor(patternLength, BASE);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            for (int i = (int)patternLength; i < (int)genomeLength; i++)
             {
                 genomeHash -= (genome[i - (int)patternLength] * factor);// % HASH_SIZE;
                 genomeHash *= BASE;
@@ -195,12 +222,17 @@ namespace Bioinformatika
                 //genomeHash = (BASE * (genomeHash - (factor * genome[i - (int)patternLength])) + genome[i]) % HASH_SIZE;
                 if (genomeHash == patternHash)
                     if (IsSubstring(genome, pattern, i - (int)patternLength + 1))
-                        patternInstances.Add(i - (int) patternLength + 1);
+                        patternInstances.Add(i - (int)patternLength + 1);
             }
+            stopwatch.Stop();
+            Console.WriteLine("The search took {0} ms or {1} timer ticks. Number of loop iterations was {2}.",
+                stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks, genomeLength - 2);
+            if (stopwatch is IDisposable)
+                (stopwatch as IDisposable).Dispose();
 
             return patternInstances;
         }
-		
+
         static ulong falsePositive = 0;
 
         private static bool IsSubstring(string genome, string pattern, int index)
@@ -213,13 +245,13 @@ namespace Bioinformatika
                 }
             return true;
         }
-		
-        private static Tuple<ulong,ulong> CalculateInitialHash(string genome, string pattern, ulong patternSize, ulong pBase)
+
+        private static Tuple<ulong, ulong> CalculateInitialHash(string genome, string pattern, ulong patternSize, ulong pBase)
         {
             ulong genomeHash = 0;
             ulong patternHash = 0;
             ulong basePower = 1;
-            for (int i = (int)patternSize - 1; i >= 0; i--, basePower*= pBase)
+            for (int i = (int)patternSize - 1; i >= 0; i--, basePower *= pBase)
             {
                 genomeHash += (genome[i] * basePower);// % HASH_SIZE;
                 patternHash += (pattern[i] * basePower);// % HASH_SIZE;
@@ -230,9 +262,9 @@ namespace Bioinformatika
         private static ulong CalculateFactor(ulong patternSize, ulong pBase)
         {
             ulong power = 1;
-            for (int i = 0; i < (int) patternSize - 1; i++)
+            for (int i = 0; i < (int)patternSize - 1; i++)
                 power = (power * pBase);// % HASH_SIZE;
             return power;
         }
-	}
+    }
 }
