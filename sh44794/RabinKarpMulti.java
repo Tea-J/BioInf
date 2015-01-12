1a1ae47ff192bfc4ea87a15aeb23d4dd500c0d2d
stuka11 <sergej_hajdin@hotmail.com>
2015-01-12 15:32:33 +0100

    RabinKarpMulti
    Supports multiple pattern search. Stores which pattern appears on which
    index.  Does not support different pattern lengths.

--- sh44794/RabinKarpMulti.java
+++ sh44794/RabinKarpMulti.java
@@ -5,6 +5,7 @@
  */
 package bioinformatika;
 
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
@@ -16,6 +17,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,13 +30,18 @@ public class RabinKarpMulti {
 
     private byte[] genomeData = null;
     private HashClass genomeHash = null;
+    private boolean sequanceHashesEqualLength = true;
+    private List<HashClass> genomeHashList = new ArrayList<>();
     private List<byte[]> sequenceDataList = new ArrayList<>();
-    private List<HashClass> sequanceHashList = new ArrayList<>();
+    private List<HashClass> sequanceHashList = new ArrayList<HashClass>();
+    private int sequenceSize;
+    private int genomeSize;
     private Path[] sequencePathArray = null;
     private Path genomePath = null;
     private int hashHits = 0;
     private int actualHits = 0;
     List<Integer> indexHitList = new ArrayList<>();
+    List<Point> indexPatternHitList = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -124,6 +131,7 @@ public class RabinKarpMulti {
     void readFiles() {
         try {
             genomeData = Files.readAllBytes(genomePath);
+            genomeSize = genomeData.length;
             for (Path p : sequencePathArray) {
                 sequenceDataList.add(Files.readAllBytes(p));
             }
@@ -131,60 +139,120 @@ public class RabinKarpMulti {
             Logger.getLogger(RabinKarp.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Error reading from files!");
         }
+        sequenceSize = sequenceDataList.get(0).length;
+        for (int i = 1; i < sequenceDataList.size(); ++i) {
+            if (sequenceDataList.get(i).length != sequenceSize) {
+                sequanceHashesEqualLength = false;
+            }
+        }
     }
 
     void runRabinKarp() {
+        int i = 0;
+
+        initHash();
+
+        while (i < (genomeSize - sequenceSize)) {
+            for (int j = 0; j < sequanceHashList.size(); ++j) {
+                HashClass h = sequanceHashList.get(j);
+                if (genomeHash.equals(h)) {
+                    hashHits++;
+
+                    if (checkPattern(i, j)) {
+                        actualHits++;
+                        indexPatternHitList.add(new Point(i, j));
+                        //vrati odmah da ima ili broji kolko ih ima ili spremi pocetne indexe svakog ponavljanja
+                    }
+                }
+            }
+        }
+        hash(++i);
+
+        // check last sequence 
+        for (int j = 0; j < sequanceHashList.size(); ++j) {
+            HashClass h = sequanceHashList.get(j);
+            if (genomeHash.equals(h)) {
+                hashHits++;
+                if (checkPattern(i, j)) {
+                    actualHits++;
+                    indexPatternHitList.add(new Point(i, j));
+                }
+            }
+        }
 
     }
 
     void hash(int start) {
-//        byte oldByte = genomeData[start - 1];
-//        byte newByte = genomeData[start + sequenceSize - 1];
-//
-//        genomeSum = genomeSum - oldByte + newByte;
-//        genomeMull = genomeMull - oldByte * sequenceSize + genomeSum;
+        genomeHash.nextHash(genomeData[start - 1], genomeData[start + sequenceSize - 1], sequenceSize);
     }
 
     void initHash() {
-        
-//        for (int i = 0; i < sequenceSize; ++i) {
-//            sequenceSum += sequenceData[i];
-//            sequenceMull += (sequenceSize - i) * sequenceData[i];
-//            genomeSum += genomeData[i];
-//            genomeMull += (sequenceSize - i) * genomeData[i];
-//        }
+        long patMull = 0;
+        int patSum = 0;
+
+        for (int i = 0; i < sequenceSize; ++i) {
+            patSum += genomeData[i];
+            patMull += (sequenceSize - i) * genomeData[i];
+        }
+        genomeHash = new HashClass(patMull, patSum);
+
+        for (int i = 0; i < sequenceDataList.size(); ++i) {
+            patMull = 0;
+            patSum = 0;
+            byte[] pattern = sequenceDataList.get(i);
+            for (int j = 0; j < pattern.length; ++j) {
+                patSum += pattern[i];
+                patMull += (sequenceSize - i) * pattern[i];
+            }
+            sequanceHashList.add(new HashClass(patMull, patSum));
+        }
     }
 
-    boolean checkPattern(int start) {
-//        for (int i = 0; i < sequenceSize; ++i) {
-//            if (sequenceData[i] != genomeData[start + i]) {
-//                return false;
-//            }
-//        }
+    boolean checkPattern(int start, int pat) {
+        byte[] pattern = sequenceDataList.get(pat);
+        for (int i = 0; i < pattern.length; ++i) {
+            if (pattern[i] != genomeData[start + i]) {
+                return false;
+            }
+        }
         return true;
+
     }
+
 }
 
 class HashClass {
 
-    int mull;
-    int sum;
+    private long mull;
+    private int sum;
 
-    HashClass(int mull, int sum) {
+    HashClass(long mull, int sum) {
         this.mull = mull;
         this.sum = sum;
     }
 
     @Override
-    public boolean equals(Object o) {        
-        return super.equals(o); //To change body of generated methods, choose Tools | Templates.
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof HashClass)) {
+            return false;
+        }
+
+        HashClass h = (HashClass) o;
+        return (this.mull == h.mull && this.sum == h.sum);
     }
 
     @Override
     public int hashCode() {
-        return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
+        int l = (int) (mull & 0x0FFFF) << 16;
+        return l | (sum & 0x0FFFF);
     }
-    
-    
-    
+
+    public void nextHash(byte oldByte, byte newByte, int size) {
+        sum = sum - oldByte + newByte;
+        mull = mull - oldByte * size + sum;
+    }
+
 }
