4956fe40cd3ec24fed8b3c328552006141b0bbb4
franosincic <frano-kresimir.sincic@fer.hr>
2015-01-13 20:41:08 +0100

    rabin karp
    dodane provjere

--- fs45825/rabin_karp.py
+++ fs45825/rabin_karp.py
@@ -4,9 +4,6 @@ import sys
 import os
 import time
 
-#from memory_profiler import profile
-
-#@profile
 def openFile(argv):
 
     with open (argv[0], "r") as genomeFile:
@@ -29,6 +26,17 @@ def openFile(argv):
                 sequenceData.append(str)
             break
     sequenceFile.close()
+
+    n = len(sequenceData[0])
+    for i in range(len(sequenceData)):
+        if (len(sequenceData[i]) != n):
+            print ("Sequences of different sizes!")
+            exit()
+
+    if (n > len(genomeData)):
+        print ("Sequences are bigger than genom!")
+        exit()
+
     return genomeData, sequenceData
 
 def robinKarpAlgorithm(genomeData, sequenceData):
@@ -54,13 +62,12 @@ def robinKarpAlgorithm(genomeData, sequenceData):
         sumHash += ord(genomeData[i])
         mulHash += (m-i)*ord(genomeData[i])
 
-
     for i in range(n - m + 1):
         for j in range(k):
             if((sumHash_pattern[j] == sumHash) and (mulHash_pattern[j] == mulHash)):
-                broj += 1
+                #broj += 1
                 if (str(sequenceData[j]) == str(genomeData[i:i+m])):
-                    result.append(i)
+                    result.append([i,j])
         if i < n -m:
             sumHash = sumHash - ord(genomeData[i]) + ord(genomeData[i + m])
             mulHash = mulHash - m*ord(genomeData[i]) + sumHash
@@ -71,18 +78,19 @@ def robinKarpAlgorithm(genomeData, sequenceData):
 if __name__ == "__main__":
     start = time.time()
     if(len(sys.argv) != 3):
-        print ("Fale argumenti!!!")
+        print ("Less arguments than needed!")
+        exit()
     if((os.stat(sys.argv[1]).st_size == 0) or (os.stat(sys.argv[2]).st_size == 0)):
-        print ("Prazni nizovi")
-    #if(os.stat(sys.argv[1]).st_size < os.stat(sys.argv[2]).st_size):
-    #    print ("Podniz veci od niza!!!")
+        print ("Empty files!")
+        exit()
 
     genomeData, sequenceData = openFile(sys.argv[1:])
 
     result, broj= robinKarpAlgorithm(genomeData, sequenceData)
 
-    print ("broj: " + str(broj))
-    print ("result: " + str(result))
-
+    #print ("broj: " + str(broj))
+    #print ("result: " + str(result))
+    for i in range(len(result), 0, -1):
+        print ("Pattern number: %d is found on %d position!" % (result[i - 1][1] + 1,result[i - 1][0] + 1))
     end = time.time()
     print (end - start)
